66dc57c9e3f89101506015afea1763c93e337cfc
Mordziarz <mazdziarzm@gmail.com>
2026-05-04 11:10:30 +0200

    Update CYTB_logic.R

--- R/CYTB_logic.R
+++ R/CYTB_logic.R
@@ -68,7 +68,7 @@ F129L=129,Y132C=132,G143A=143
       genome_path = f_path,
       forward_primers = c(CYTB_F),
       reverse_primers = c(CYTB_R),
-      max_mismatch = 2,
+      min_contig_length = 500,
       ...
     )
     
