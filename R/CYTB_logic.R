86b7ef70b40a1c8f7e94e5b82ff2be4677739016
Mordziarz <mazdziarzm@gmail.com>
2026-05-04 13:28:08 +0200

    Update CYTB_logic.R

--- R/CYTB_logic.R
+++ R/CYTB_logic.R
@@ -18,11 +18,11 @@ get_CYTB <- function(input_path, output_dir = "zymor_results", ...) {
 CYTB_R <- "ATTGTTCATGTTAATGTTCT"
 CYTB_F <- "AGCCATAACATAATTCTCGC"
 #here
-CYTB_reference <- Biostrings::DNAString("TTATTTTACGTTATTTGTGTTGTTTAAGTGCATATCCACTAAAGTATTCTCGATTAAGCTTACTAGAGGCACTATGATTAAGAAATGTGAAAAGTACAGTACTGTACTAATTTGCCCAAATTCTATAAATGGTGACTCAACGTGTTTAGCACCTAATTGCATTAATACTAAGAAGTTAGCAACAAATATGTAAAATGTGATCTTGCTTAAAGGTCTAAATTGTAATCCTCTACTTCTACCTAAATCGGTAATTGGCATTATCATTATAATCAATATAGCAGAAAACATTGCGATCACACCTAATAGTTTGTTAGGTATTGATCTTAGTATAGCGTAGAAAGGTAATAAATATCATTCAGGCACTATAGCAGGTGGAGTTTGCATAGGGTTAGCCATAACATAATTCTCGCTGTCACCTAAAACGTTAGGCATAAAGAAAACAAATATTGATAACACTATAATGAATAAAAATATTGTTATTAAATCTTTGAATATAAAGTAAGGGGCGAATGGTAATCTATCGTAGTTACCTGATACACCTAAAGGATTTCCTGAACCCGCTGTATCGTGTAAAGCTATTAGATGCATTAAAACTAATGCAGCTAACACAAACGGTAAAACGAAATGTAGAGCAAAGAATCTGTTCAATGTTGCATTGTTAACAGAAAATCCACCTCATACGAATTCAACTATGTCTTGTCCAACTCAAGGTATTGCACTCAATAAGTTAGTTATAACTGTTGCTCCTCATAAAGACATTTGACCATAAGGTAATACATACCCTAAGAATGCGGTTGCCATCATCAGAACTAGTATTATAGTACCGATTGTTCATGTTAATGTTCTAGGGGCTTTGTATGACCCGTAGTATAACCCTCTACCCACGTGCAGGTAAACTAAAAAGAAGAAAGCTGAAGCAGTATTAGAGTGAAGATAACGTATAAGTCATCCATTGTTTACATCTCTCATTATGTGTTCAACTGAGTTAAATGCTTCAGATACGCTAGGGTTATAATGCATAGCAAGTGTAACACCAGTAACGATTTGTATTACAAGACAAAATCCCAGTAATGAACCGAAATTTCATAGATAGCTTAGATTACTTGGTTGTGGTGAATCGATAAGATAACCATTAACCAGACTAAATAAAGGGTGACTCTTTCAAATCCTCAT")
+CYTB_reference <- Biostrings::DNAString("AGCCATAACATAATTCTCGCTGTCACCTAAAACGTTAGGCATAAAGAAAACAAATATTGATAACACTATAATGAATAAAAATATTGTTATTAAATCTTTGAATATAAAGTAAGGGGCGAATGGTAATCTATCGTAGTTACCTGATACACCTAAAGGATTTCCTGAACCCGCTGTATCGTGTAAAGCTATTAGATGCATTAAAACTAATGCAGCTAACACAAACGGTAAAACGAAATGTAGAGCAAAGAATCTGTTCAATGTTGCATTGTTAACAGAAAATCCACCTCATACGAATTCAACTATGTCTTGTCCAACTCAAGGTATTGCACTCAATAAGTTAGTTATAACTGTTGCTCCTCATAAAGACATTTGACCATAAGGTAATACATACCCTAAGAATGCGGTTGCCATCATCAGAACTAGTATTATAGTACCGATTGTTCATGTTAATGTTCT")
 
 CYTB_CDS <- IRanges::IRanges(
   start = c(1),
-  end   = c(1173),
+  end   = c(456),
   names = c("CDS1")
 )
   
@@ -148,86 +148,74 @@ F129L=129,Y132C=132,G143A=143
   return(final_table)
 }
 
-.internal_cytb_processing <- function(analyze_genome_results, CYTB_reference, cds_ranges, CYTB_target_positions) {
+.internal_cytb_processing <- function(analyze_genome_results, CYTB_reference, cds_ranges, CYTB_target_positions) { 
   
-  get_neg_pos <- function(nt_idx, ranges) {
-    all_pts <- c()
-    for(i in seq_along(ranges)) {
-      all_pts <- c(all_pts, seq(end(ranges[i]), start(ranges[i])))
-    }
-    return(all_pts[nt_idx])
-  }
+  get_neg_pos <- function(nt_idx, ranges) { 
+    all_pts <- c() 
+    for(i in seq_along(ranges)) { 
+      all_pts <- c(all_pts, seq(end(ranges[i]), start(ranges[i]))) 
+    } 
+    return(all_pts[nt_idx]) 
+  } 
   
-  check_mut <- function(amp, ref, t_pos) {
-    if (length(amp) == 0) return(list(aa = "EMPTY"))
+  check_mut <- function(amp, ref, t_pos) { 
+    aln <- pwalign::pairwiseAlignment( 
+      pattern = DNAString(as.character(amp)),  
+      subject = DNAString(as.character(ref)),  
+      type = "global-local" 
+    ) 
     
-    amp_dna <- Biostrings::DNAString(as.character(amp))
+    sub_aln <- as.character(pwalign::subject(aln)) 
+    pat_aln <- as.character(pwalign::pattern(aln)) 
+    ref_idx <- which(strsplit(sub_aln, "")[[1]] != "-") 
     
-    start_match <- Biostrings::matchPattern(
-      pattern = Biostrings::subseq(amp_dna, 1, 20),
-      subject = ref
-    )
+    if(max(t_pos) > length(ref_idx)) return(list(aa = "EMPTY")) 
     
-    if (length(start_match) == 0) {
-      return(list(aa = "EMPTY"))
-    }
+    t_aln <- ref_idx[t_pos] 
+    pat_vec <- strsplit(pat_aln, "")[[1]] 
+    raw_ex <- paste0(pat_vec[t_aln], collapse = "") 
     
-    offset <- start(start_match)[1] - 1
+    if(nchar(raw_ex) < 3 || grepl grepl("-", raw_ex)) return(list(aa = "DEL")) 
     
-    amp_pos <- t_pos - offset
+    codon_dna <- Biostrings::complement(DNAString(raw_ex)) 
     
-    if (min(amp_pos) < 1 || max(amp_pos) > length(amp_dna)) {
-      return(list(aa = "EMPTY"))
-    }
+    amino <- as.character(Biostrings::translate( 
+      codon_dna,  
+      genetic.code = Biostrings::getGeneticCode("1"),  
+      if.fuzzy.codon = "solve", 
+      no.init.codon = TRUE 
+    )) 
     
-    codon_nt <- as.character(Biostrings::subseq(amp_dna, start = min(amp_pos), width = 3))
-    
-    if (nchar(codon_nt) < 3) {
-      return(list(aa = "DEL"))
-    }
-
-    codon_dna <- Biostrings::complement(Biostrings::DNAString(codon_nt))
-    
-    amino <- as.character(Biostrings::translate(
-      codon_dna, 
-      genetic.code = Biostrings::getGeneticCode("1"), 
-      if.fuzzy.codon = "solve",
-      no.init.codon = TRUE
-    ))
-    
-    return(list(aa = amino))
-  }
+    return(list(aa = amino)) 
+  } 
   
-  mut_names <- names(CYTB_target_positions)
-  col_names <- c("Sample_ID", mut_names)
+  mut_names <- names(CYTB_target_positions) 
+  col_names <- c("Sample_ID", mut_names) 
   
-  res_table <- data.frame(matrix(NA, nrow = length(analyze_genome_results), ncol = length(col_names)))
-  colnames(res_table) <- col_names
+  res_table <- data.frame(matrix(NA, nrow = length(analyze_genome_results), ncol = length(col_names))) 
+  colnames(res_table) <- col_names 
   
-  for (i in seq_along(analyze_genome_results)) {
-    amp_data <- analyze_genome_results[[i]]
-    curr_amp <- amp_data[["with_p"]]
-    curr_id  <- as.character(amp_data[["amplicon_id"]])
+  for (i in seq_along(analyze_genome_results)) { 
+    amp_data <- analyze_genome_results[[i]] 
+    curr_amp <- amp_data[["with_p"]] 
+    curr_id  <- as.character(amp_data[["amplicon_id"]]) 
     
-    if (is.null(curr_id) || nchar(curr_id) == 0) {
-      curr_id <- paste0("sample_", i)
-    }
-    res_table[i, "Sample_ID"] <- curr_id
+    res_table[i, "Sample_ID"] <- curr_id 
     
-    for (m_name in mut_names) {
-      aa_num <- CYTB_target_positions[[m_name]]
-      dna_idx <- get_neg_pos(c((aa_num*3)-2, (aa_num*3)-1, (aa_num*3)), cds_ranges)
+    for (m_name in mut_names) { 
+      aa_num <- CYTB_target_positions[[m_name]] 
+      dna_idx <- get_neg_pos(c((aa_num*3)-2, (aa_num*3)-1, (aa_num*3)), cds_ranges) 
       
-      mut_res <- check_mut(curr_amp, CYTB_reference, dna_idx)
-      wild_aa <- substr(m_name, 1, 1)
+      mut_res <- check_mut(curr_amp, CYTB_reference, dna_idx) 
+      wild_aa <- substr(m_name, 1, 1) 
       
-      if (mut_res$aa != "EMPTY" && mut_res$aa != wild_aa) {
-        res_table[i, m_name] <- mut_res$aa
-      } else {
-        res_table[i, m_name] <- "wt"
-      }
-    }
-  }
+      if (mut_res$aa != wild_aa && mut_res$aa != "EMPTY") { 
+        res_table[i, m_name] <- mut_res$aa 
+      } else { 
+        res_table[i, m_name] <- "wt" 
+      } 
+    } 
+  } 
   
-  return(res_table)
+  return(res_table) 
 }
