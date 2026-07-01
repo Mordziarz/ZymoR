2263a4c8abc2b5f641c059e444a4e2421c0fc291
Mordziarz <mazdziarzm@gmail.com>
2026-07-02 00:29:10 +0200

    Update CYTB_amplicon.R

--- R/CYTB_amplicon.R
+++ R/CYTB_amplicon.R
@@ -52,6 +52,8 @@ get_CYTB_amplicon <- function(input_bam) {
       which = GRanges("CYTB", IRanges(1, 200))
     ))
     
+    if (length(aln) == 0) next
+    
     if ("cigarillo" %in% loadedNamespaces() || requireNamespace("cigarillo", quietly = TRUE)) {
       seqs_aligned <- cigarillo::project_sequences(mcols(aln)$seq, cigar(aln), from = "query", to = "reference", D.letter = "-", N.letter = "N")
     } else {
@@ -64,20 +66,24 @@ get_CYTB_amplicon <- function(input_bam) {
     
     for (m_name in target_names) {
       pos <- CYTB_target_positions[[m_name]]
-      wt_aa <- substr(m_name, 1, 1)          # F
-      mut_aa <- substr(m_name, nchar(m_name), nchar(m_name)) # L
+      wt_aa <- substr(m_name, 1, 1)
       
       idx_glob <- full_map[c((pos*3)-2, (pos*3)-1, (pos*3))]
-      l1 <- idx_glob[1] - read_starts + 1; l2 <- idx_glob[2] - read_starts + 1; l3 <- idx_glob[3] - read_starts + 1
-      in_range <- (l1 >= 1 & l3 <= seq_widths)
+      
+      l1 <- idx_glob[1] - read_starts + 1
+      l2 <- idx_glob[2] - read_starts + 1
+      l3 <- idx_glob[3] - read_starts + 1
+      
+      in_range <- (l1 >= 1 & l2 >= 1 & l3 >= 1 & l1 <= seq_widths & l2 <= seq_widths & l3 <= seq_widths)
       
       res_vec <- rep("OUT", length(aln))
       valid_idx <- which(in_range)
       
       if (length(valid_idx) > 0) {
-        c1 <- subseq(seqs_aligned[valid_idx], l1[valid_idx], l1[valid_idx])
-        c2 <- subseq(seqs_aligned[valid_idx], l2[valid_idx], l2[valid_idx])
-        c3 <- subseq(seqs_aligned[valid_idx], l3[valid_idx], l3[valid_idx])
+        c1 <- subseq(seqs_aligned[valid_idx], start = l1[valid_idx], end = l1[valid_idx])
+        c2 <- subseq(seqs_aligned[valid_idx], start = l2[valid_idx], end = l2[valid_idx])
+        c3 <- subseq(seqs_aligned[valid_idx], start = l3[valid_idx], end = l3[valid_idx])
+        
         codons <- paste0(c1, c2, c3)
         
         is_del <- grepl("-", codons)
@@ -99,9 +105,7 @@ get_CYTB_amplicon <- function(input_bam) {
     
     is_new <- apply(res_dt[, ..target_names], 1, function(x) {
       any(sapply(seq_along(x), function(i) {
-        val <- x[i]
-        m_name <- target_names[i]
-        mut_char <- substr(m_name, nchar(m_name), nchar(m_name))
+        val <- x[i]; mut_char <- substr(target_names[i], nchar(target_names[i]), nchar(target_names[i]))
         !(val == "wt" | val == mut_char | val == "OUT")
       }))
     })
@@ -112,7 +116,6 @@ get_CYTB_amplicon <- function(input_bam) {
     
     sample_summary <- res_dt[, .(N = .N), by = c("Status", target_names)]
     sample_summary[, Sample_ID := sample_name]
-    
     setcolorder(sample_summary, c("Sample_ID", "Status", "N", target_names))
     final_results <- rbind(final_results, sample_summary)
   }
